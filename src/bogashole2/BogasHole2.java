271bdee8d510411fc852be4463a4e11e95966308
Linkadd <34799196+linkadd515@users.noreply.github.com>
2020-12-03 15:44:58 +0000

    initial commit

---
+++ src/bogashole2/BogasHole2.java
@@ -0,0 +1,15 @@
+
+package bogashole2;
+
+public class BogasHole2 {
+
+    public static void main(String[] args) {
+        BogasHoleP p = new BogasHoleP("aaaahhh",8453,true);
+    
+        p.hi();
+        
+        
+    }
+    
+}
+    
---
+++ src/bogashole2/BogasHoleP.java
@@ -0,0 +1,20 @@
+package bogashole2;
+
+public class BogasHoleP {
+    
+    
+    private String S1;
+    private int I1;
+    private boolean B1;
+    
+    public BogasHoleP(String S2 , int I2, boolean B2) {
+        this.S1 = S2;
+        this.I1 = I2;
+        this.B1 = B2;
+        }
+
+    
+    public void hi(){
+        System.out.println(S1);
+    }
+}
